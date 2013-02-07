37391cc3ad77975b859ffbeaf0a711db4390e475
dharmesh <cosc480@cosc480.(none)>
2013-02-07 15:15:53 -0500

    modified part 1 and part 2

--- code/part1.rb
+++ code/part1.rb
@@ -1,6 +1,18 @@
+#/usr/bin/ruby
+
 def palindrome?(str)
-  # YOUR CODE HERE
+  # parsed_str has no spaces and is lowercase
+  parsed_str = str.gsub(/[^\w*]/, "").downcase()
+  return parsed_str == parsed_str.reverse
 end
+
+def foo()
+  puts palindrome?("A man, a plan, a canal -- Panama")
+  puts palindrome?("Madam, I'm Adam!")
+  puts palindrome?("Abracadabra")
+end
+
+foo()
 
 def count_words(str)
   # YOUR CODE HERE
--- code/part2.rb
+++ code/part2.rb
@@ -6,9 +6,44 @@ def rps_result(m1, m2)
 end
 
 def rps_game_winner(game)
-  # YOUR CODE HERE
+  if game.length != 2
+    raise WrongNumberOfPlayersError
+  end
+  
+  game.each do |player, strategy|
+    if strategy.downcase != "p" and strategy.downcase != "r" and
+        strategy.downcase != "s"
+      raise NoSuchStrategyError
+    end
+  end
+
+  player1 = game[0][0]
+  strategy1 = game[0][1]
+  player2 = game[1][0]
+  strategy2 = game[1][1]
+
+  h = Hash.new
+  h['r'] = 's'
+  h['p'] = 'r'
+  h['s'] = 'p'
+
+  if strategy1.downcase == strategy2.downcase
+    return [player1, strategy1]
+  elsif h[strategy1.downcase] == strategy2.downcase
+    return [player1, strategy1]
+  else
+    return [player2, strategy2]
+  end
 end
 
 def rps_tournament_winner(tournament)
-  # YOUR CODE HERE
+  if tournament[0][0].instance_of?("String")
+    return rps_game_winner(tournament)
+  elsif tournament[0][0].instance_of?("Array")
+    tournament.each do |tournament_instance|
+      tournament[0] = rps_tournament_winner(tournament_instance)
+    end
+  end
+
+  return rps_game_winner(tournament)
 end
